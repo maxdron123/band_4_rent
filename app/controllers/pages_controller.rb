dfb51dcd0c97b625174b52e531b12d66a10db5af
Danial Firdaus <danfird7@gmail.com>
2023-06-03 14:23:28 +0100

    about method and about html erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,14 @@ class PagesController < ApplicationController
   end
 
   def about
+    @app_name = 'Bands 4 Rent'
+    @app_description = 'Welcome to Bands 4 Rent, the platform where venues can connect with bands and book live performances.'
+    @app_features = [
+      'Browse and search for bands available for live performances.',
+      'View detailed profiles and performance samples of bands.',
+      'Contact bands directly to inquire about booking and availability.',
+      'Manage bookings and schedule performances seamlessly.'
+    ]
   end
 
   def contact
