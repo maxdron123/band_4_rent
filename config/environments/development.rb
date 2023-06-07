3b363e66b42e8155ec09407f06533a23e971ed07
Maksym Dronov <simbanyan98@gmail.com>
2023-06-07 20:48:07 +0300

    fixed edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_storage.service = :cloudinary
+  config.active_storage.replace_on_assign_to_many = false
 end
